738b2c1b2c2b2a35dd6f0aa1ecd890fee8dc388c
acreter <acreter@insmother>
2020-12-14 18:36:20 +0100

    Preparing for rewrite

--- sldrawer.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SLDRAWER_H_INCLUDED
-#define _SLDRAWER_H_INCLUDED
-#include <slsolver.h>
-
-void storylines_draw_solution(Storylines*, Solution, char* filename, char* type);
-#endif
--- src/solution.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SOLUTION_H_INCLUDED
-#define _SOLUTION_H_INCLUDED
-#include <acvector.h>
-#include "structs.h"
-
-acVector* solution_add_seperators_single_layer(acVector*, acVector*, Entity*);
-#endif
